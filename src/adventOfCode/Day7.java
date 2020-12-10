9900e23e10981d578f26ff7e88a41e99a9667292
user04 <user04@oem-HP-EliteDesk-800-G5-Desktop-Mini>
2020-12-10 09:40:28 -0700

    finnished day 7 of aoc

--- src/adventOfCode/Day7.java
+++ src/adventOfCode/Day7.java
@@ -6,57 +6,65 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day7 {
-	
-	public static HashMap<String, String> dict = new HashMap<String,String>();
-	public static int gold = 0, numG = 0;
+
+	public static HashMap<String, String> dict = new HashMap<String, String>();
+	public static int gold = 0;
+	public static long numG = 0;
 	public static boolean counted = false;
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		File f = new File("src/adventOfCode/Day7.txt");
 		Scanner s = new Scanner(f);
-		while(s.hasNext()) {
+		while (s.hasNext()) {
 			String temp = s.nextLine();
-			dict.put(temp.substring(0, temp.indexOf(" contain")), temp.substring(temp.indexOf("contain")+8, temp.length()-1));
+			dict.put(temp.substring(0, temp.indexOf(" contain")),
+					temp.substring(temp.indexOf("contain") + 8, temp.length() - 1));
 		}
 		dict.forEach((k, v) -> {
 			counted = false;
-            hasGold(k);
-        });
-		countGold("shiny gold bags");
+			hasGold(k);
+		});
+		countGold("shiny gold bags", 1);
 		System.out.println(gold);
 		System.out.println(numG);
 		s.close();
 	}
-	
+
 	public static void hasGold(String key) {
 		String value = dict.get(key);
-		if(counted) {
+		if (counted) {
 			return;
 		}
-		if(value.contains("shiny gold bag")) {
+		if (value.contains("shiny gold bag")) {
 			gold++;
 			counted = true;
 			return;
-		}
-		else if(value.equals("no other bags")) {
+		} else if (value.equals("no other bags")) {
 			return;
-		}
-		else {
+		} else {
 			String[] p = value.split(", ");
-			for(int i = 0; i < p.length; i++) {
-				if(!p[i].substring(p[i].length()-1).equals("s")) {
+			for (int i = 0; i < p.length; i++) {
+				if (!p[i].substring(p[i].length() - 1).equals("s")) {
 					p[i] += "s";
 				}
-				hasGold(p[i].substring(p[i].indexOf(' ')+1));
+				hasGold(p[i].substring(p[i].indexOf(' ') + 1));
 			}
 		}
 	}
-	
-	public static void countGold(String key) {
+
+	public static void countGold(String key, int mult) {
 		String value = dict.get(key);
+		if (value.equals("no other bags")) {
+			return;
+		}
 		String[] p = value.split(", ");
-		for(int i = 0; i < p.length; i++) {
-			
+		for (int i = 0; i < p.length; i++) {
+			if (!p[i].substring(p[i].length() - 1).equals("s")) {
+				p[i] += "s";
+			}
+			numG += mult * Integer.parseInt(p[i].substring(0, p[i].indexOf(' ')));
+			countGold(p[i].substring(p[i].indexOf(' ') + 1),
+					mult * Integer.parseInt(p[i].substring(0, p[i].indexOf(' '))));
 		}
 	}
 
